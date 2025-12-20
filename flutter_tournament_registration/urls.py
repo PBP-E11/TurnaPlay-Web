c9e273aa5c923732f952aee3a3db6c63e3017acf
SeeStarz <mfahrim7@gmail.com>
2025-12-21 00:57:57 +0700

    Tournament registration API

--- flutter_tournament_registration/urls.py
+++ flutter_tournament_registration/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
-app_name = 'flutter_tournament_registration'
+app_name = 'flutter_team'
 
 urlpatterns = [
-
+    path('create', views.create_team, name='create_team'),
+    path('details', views.get_team, name='get_team'),
+    path('update', views.update_team, name='update_team'),
+    path('delete', views.delete_team, name='delete_team'),
+    path('member/join', views.join_member, name='join_member'),
+    path('member/update', views.update_member, name='update_member'),
+    path('member/delete', views.delete_member, name='delete_member'),
 ]
