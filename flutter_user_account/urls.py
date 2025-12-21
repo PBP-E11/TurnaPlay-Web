4a56009787b32f8f1199e84a64e1c36b6380df91
Wahyuga <tawahyuga17@gmail.com>
2025-12-21 15:45:28 +0700

    feat: add logout path to user account URLs

--- flutter_user_account/urls.py
+++ flutter_user_account/urls.py
@@ -6,6 +6,7 @@ app_name = 'flutter_user_account'
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
     # Dashboard Admin - User
     path('dashboard/stats/', views.dashboard_stats, name='dashboard_stats'),
     path('dashboard/users/',  views.list_users, name='list_users'),
