40d21ca0b92ec662ef24aceed944df27fcbbd27e
Wahyuga <tawahyuga17@gmail.com>
2025-12-21 13:41:34 +0700

    Revert "Merge branch 'dev' of https://github.com/PBP-E11/TurnaPlay into feat/flutter-user-account"
    This reverts commit eb1893beec086e63dc6dde0088f734963604122b, reversing
    changes made to 3217d5212bce7e07f46a68b7fa00768ba91575d5.

--- flutter_game_account/urls.py
+++ flutter_game_account/urls.py
@@ -4,7 +4,5 @@ from . import views
 app_name = 'flutter_game_account'
 
 urlpatterns = [
-    path('', views.game_accounts_list_create, name='gameaccount-list-create'),
-    path('<uuid:pk>/', views.GameAccountDetail.as_view(), name='gameaccount-detail'),
-    path('games/', views.games_list, name='game-list'),
+
 ]
--- flutter_tournament_registration/urls.py
+++ flutter_tournament_registration/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'flutter_team'
+app_name = 'flutter_tournament_registration'
 
 urlpatterns = [
-    path('create/', views.create_team, name='create_team'),
-    path('details/', views.get_team, name='get_team'),
-    path('update/', views.update_team, name='update_team'),
-    path('delete/', views.delete_team, name='delete_team'),
-    path('member/join/', views.join_member, name='join_member'),
-    path('member/update/', views.update_member, name='update_member'),
-    path('member/delete/', views.delete_member, name='delete_member'),
+
 ]
